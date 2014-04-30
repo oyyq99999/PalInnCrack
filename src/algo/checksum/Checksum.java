e893dcf3cb528368c5af78694df4b698673938d6
Yunqi Ouyang <oyyq9999999@163.com>
2014-04-30 17:41:53 +0800

    initial commit

---
+++ src/algo/checksum/Checksum.java
@@ -0,0 +1,122 @@
+package algo.checksum;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.spec.KeySpec;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+public class Checksum {
+    
+    /**
+     * 被加密的字符串
+     */
+    private static final String HOLY_SHIT = "Des for PalInn";
+
+    /**
+     * 计算存档文件的校验和，以此作为加密的key
+     * @param saveFile 存档文件
+     * @return 校验和的字符串形式
+     */
+    private static String calcChecksum(File saveFile) {
+        try (FileInputStream fis = new FileInputStream(saveFile)) {
+            long length = saveFile.length();
+            long checksum = 0;
+            while (length-- > 0) {
+                int one = fis.read();
+                checksum += one;
+            }
+            return checksum + "";
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static byte[] encrypt(byte[] src) throws Exception {
+        byte[] data = new byte[16];
+        KeySpec ks = new DESKeySpec(src);
+        SecretKeyFactory kf = SecretKeyFactory.getInstance("DES");
+        SecretKey key = kf.generateSecret(ks);
+        Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        for (int i = 0; i < HOLY_SHIT.length(); i++) {
+            data[i] = (byte) HOLY_SHIT.charAt(i);
+        }
+        byte[] res = cipher.doFinal(data);
+        return res;
+    }
+
+    private static void save(String got, File crcFile) throws Exception {
+        if (crcFile.exists()) {
+            backup(crcFile);
+        }
+        FileOutputStream fos = new FileOutputStream(crcFile);
+        byte[] bytes = new byte[got.length() + 2];
+        bytes[0] = (byte) got.length();
+        for (int i = 0; i < got.length(); i++) {
+            bytes[i + 2] = (byte) got.charAt(i);
+        }
+        fos.write(bytes);
+        fos.close();
+    }
+
+    private static void backup(File crcFile) {
+        String oldFilename = crcFile.getAbsolutePath();
+        String newFilename = oldFilename
+                + (new SimpleDateFormat("'.'yyyyMMddHHmm").format(new Date()));
+        Runtime rt = Runtime.getRuntime();
+        try {
+            rt.exec(String.format("cmd /c copy %s %s", oldFilename, newFilename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String saveFile = args[0];
+        String crcFile = null;
+        if (args.length > 1) {
+            crcFile = args[1];
+        } else {
+            crcFile = saveFile.replaceFirst("\\.[^.]+$", ".crc");
+        }
+        encrypt(saveFile, crcFile);
+    }
+
+    public static int encrypt(String saveFile, String crcFile) {
+        return encrypt(new File(saveFile), new File(crcFile));
+    }
+
+    public static int encrypt(File saveFile, File crcFile) {
+        try {
+            String sum = calcChecksum(saveFile);
+            byte[] bytes = sum.getBytes();
+            byte[] key = new byte[8];
+            for (int i = 0; i < bytes.length && i < 8; i++) {
+                key[i] = bytes[i];
+            }
+            byte[] res = encrypt(key);
+            String got = "";
+            for (int i = 0; i < res.length; i++) {
+                got += String.format("%02X", res[i] & 0xFF);
+            }
+            save(got, crcFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return 0;
+    }
+
+}
---
+++ src/ui/gui/MainGui.java
@@ -0,0 +1,247 @@
+package ui.gui;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import algo.checksum.Checksum;
+
+public class MainGui extends JFrame implements ActionListener {
+
+    private static final long serialVersionUID = 5517819703253784310L;
+    private static final int  COLUMN_WIDTH     = 40;
+
+    public static void main(String[] args) throws Exception {
+        new MainGui();
+    }
+
+    JButton    action;
+    JPanel     crcFile;
+    JButton    crcFileButton;
+
+    JLabel     crcFileLabel;
+    JTextField crcFilePath;
+    JPanel     saveFile;
+    JButton    saveFileButton;
+
+    JLabel     saveFileLabel;
+
+    JTextField saveFilePath;
+
+    String     systemCharsetName = System.getProperty("sun.jnu.encoding");
+    String     defaultPath       = "";
+
+    private MainGui() {
+        initData();
+        initSelf();
+        initComponents();
+        addActions();
+        showSelf();
+    }
+
+    private void initData() {
+        String system = System.getProperty("os.name", "unknown");
+        if (system == null || !system.toLowerCase().contains("windows")) {
+            return;
+        }
+        Runtime rt = Runtime.getRuntime();
+        try {
+            Process process = rt
+                    .exec("cmd /c reg query HKLM\\Software\\大宇资讯集团软星科技(北京)有限公司\\仙剑客栈\\1.00.000");
+            BufferedInputStream bis = new BufferedInputStream(process.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(bis, systemCharsetName));
+            String line = null;
+            Pattern pattern = Pattern.compile("^\\s*TargetPath\\s+REG_SZ\\s+(.*)\\s*$");
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    defaultPath = matcher.group(1);
+                    return;
+                }
+            }
+
+            process = rt.exec("cmd /c reg query HKCU\\Software\\SOFTSTAR\\PalInn");
+            bis = new BufferedInputStream(process.getInputStream());
+            br = new BufferedReader(new InputStreamReader(bis, systemCharsetName));
+            line = null;
+            pattern = Pattern.compile("^\\s*DIRECTORY\\s+REG_SZ\\s+(.*)\\s*$");
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    defaultPath = matcher.group(1);
+                    return;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addActions() {
+        saveFilePath.addActionListener(this);
+        saveFileButton.addActionListener(this);
+        crcFileButton.addActionListener(this);
+        action.addActionListener(this);
+    }
+
+    private void chooseCrcFile() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("选择加密文件");
+        chooser.setCurrentDirectory(new File(defaultPath));
+        chooser.setAcceptAllFileFilterUsed(false);
+        chooser.setFileFilter(new FileNameExtensionFilter("仙剑存档加密文件(*.crc)", "crc"));
+        int result = chooser.showSaveDialog(this);
+        try {
+            if (result == JFileChooser.APPROVE_OPTION) {
+                String filename = chooser.getSelectedFile().getCanonicalPath();
+                crcFilePath.setText(filename);
+            }
+            defaultPath = chooser.getCurrentDirectory().getCanonicalPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void chooseSaveFile() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("打开存档文件");
+        chooser.setCurrentDirectory(new File(defaultPath));
+        chooser.setAcceptAllFileFilterUsed(false);
+        chooser.setFileFilter(new FileNameExtensionFilter("仙剑存档文件(*.sav)", "sav"));
+        int result = chooser.showOpenDialog(this);
+        try {
+            if (result == JFileChooser.APPROVE_OPTION) {
+                String filename = chooser.getSelectedFile().getCanonicalPath();
+                saveFilePath.setText(filename);
+                saveFilePath.postActionEvent();
+            }
+            defaultPath = chooser.getCurrentDirectory().getCanonicalPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doEncrypt() {
+        File saveFile = new File(saveFilePath.getText());
+        if (!saveFile.exists()) {
+            JOptionPane.showMessageDialog(this, "存档文件不存在！", "错误", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        File crcFile = new File(crcFilePath.getText());
+        if (crcFile.exists()) {
+            if (crcFile.getAbsolutePath().equals(saveFile.getAbsolutePath())) {
+                JOptionPane
+                        .showMessageDialog(this, "存档文件和加密文件相同！", "错误", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            int result = JOptionPane.showConfirmDialog(this, "加密文件已存在，是否覆盖？", "文件已存在",
+                    JOptionPane.OK_CANCEL_OPTION);
+            if (result == JOptionPane.CANCEL_OPTION) {
+                return;
+            }
+        }
+        int result = Checksum.encrypt(saveFile, crcFile);
+        if (result == 0) {
+            JOptionPane.showMessageDialog(this, "加密成功！", "成功", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(this, "加密失败...", "失败", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void generateDefaultCrcFile() {
+        String filename = saveFilePath.getText();
+        if (filename.toLowerCase().endsWith(".sav")) {
+            filename = filename.replaceFirst("\\.[^.]+$", ".crc");
+        } else {
+            return;
+        }
+        crcFilePath.setText(filename);
+    }
+
+    private void initComponents() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        saveFile = new JPanel(new FlowLayout());
+        saveFileLabel = new JLabel("存档文件：");
+        saveFilePath = new JTextField();
+        saveFilePath.setColumns(COLUMN_WIDTH);
+        saveFilePath.setEditable(false);
+        saveFileButton = new JButton("浏览");
+        saveFile.add(saveFileLabel);
+        saveFile.add(saveFilePath);
+        saveFile.add(saveFileButton);
+
+        crcFile = new JPanel(new FlowLayout());
+        crcFileLabel = new JLabel("CRC文件：");
+        crcFilePath = new JTextField();
+        crcFilePath.setColumns(COLUMN_WIDTH);
+        crcFilePath.setEditable(false);
+        crcFileButton = new JButton("浏览");
+        crcFile.add(crcFileLabel);
+        crcFile.add(crcFilePath);
+        crcFile.add(crcFileButton);
+
+        JPanel actionPanel = new JPanel(new FlowLayout());
+        action = new JButton("加密");
+        actionPanel.add(action);
+
+        Box box = Box.createVerticalBox();
+        box.add(saveFile);
+        box.add(crcFile);
+        box.add(actionPanel);
+
+        this.add(box);
+    }
+
+    private void initSelf() {
+        setBounds(300, 300, 480, 300);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setTitle("仙剑客栈存档加密工具");
+        setResizable(false);
+    }
+
+    private void showSelf() {
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == saveFileButton) {
+            chooseSaveFile();
+            return;
+        }
+        if (e.getSource() == saveFilePath) {
+            generateDefaultCrcFile();
+            return;
+        }
+        if (e.getSource() == crcFileButton) {
+            chooseCrcFile();
+            return;
+        }
+        if (e.getSource() == action) {
+            doEncrypt();
+        }
+    }
+
+}
