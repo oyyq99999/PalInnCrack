276e992533347b5028fd86a0787caba260af12d7
Yunqi Ouyang <oyyq9999999@163.com>
2014-07-16 02:58:51 +0800

    Squashed 'win-registry/' content from commit 1c4c02c
    git-subtree-dir: win-registry
    git-subtree-split: 1c4c02c393b7b270edb75f3b36f6073e3405d499

---
+++ src/main/java/com/github/sarxos/winreg/internal/RC.java
@@ -0,0 +1,9 @@
+package com.github.sarxos.winreg.internal;
+
+public class RC {
+
+	public static final int SUCCESS = 0;
+	public static final int NOTFOUND = 2;
+	public static final int ACCESSDENIED = 5;
+
+}
