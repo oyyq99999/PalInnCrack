586cecf7735bcf62da7a38426064a41e37550e2f
Yunqi Ouyang <oyyq9999999@163.com>
2015-01-30 18:42:16 +0800

    修改打包方式为gradle，删除eclipse项目文件

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ PalInnCrack
 ===========
 
 仙剑客栈存档加密crc文件计算工具
+
+编译时需要依赖oyyq9999/Utils和oyyq99999/win-registry
+
+使用`gradle fatJar`来打包包含依赖的jar文件
--- win-registry/src/main/java/com/github/sarxos/winreg/internal/RC.java
+++
@@ -1,9 +0,0 @@
-package com.github.sarxos.winreg.internal;
-
-public class RC {
-
-	public static final int SUCCESS = 0;
-	public static final int NOTFOUND = 2;
-	public static final int ACCESSDENIED = 5;
-
-}
